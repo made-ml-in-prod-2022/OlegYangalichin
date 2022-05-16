20e43d8c63cc882e68cc6076faae4f920055b2cb
Oleg Yangalichin <36154304+olegyang@users.noreply.github.com>
2022-05-16 23:08:10 +0300

    Delete ml_project directory

--- ml_project/ml_example/params/data_split_params.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass()
-class SplittingParams:
-    split_size: float
-    random_state: int
-    shuffle: bool
--- ml_project/ml_example/params/training_params.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-from dataclasses import dataclass, field
-
-
-@dataclass()
-class TrainingParams:
-    model: str
-    model_params: Optional[dict]
